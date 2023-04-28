a4b8716f9d062e9b732161cfaf1923fc548084d1
stethbo <sztefan.borek@gmail.com>
2023-04-28 12:00:51 +0200

    copies nessesary files from stefan-dev

---
+++ src/config.py
@@ -0,0 +1,7 @@
+import os
+import sys
+
+sys.path.append('../')
+
+DATASET_PATH = os.path.join("data", "VOC_for_NLP.xlsx")
+POLISH_STOPWORDS_PATH = os.path.join("..", "data", "polish_stopwords.txt")
