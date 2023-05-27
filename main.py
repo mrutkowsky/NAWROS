6feac49bbd1ee761397c9b9adc236963ea9a6b41
Jushida <98091655+Jushida@users.noreply.github.com>
2023-05-27 23:57:25 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,39 +2,35 @@ import pandas as pd
 import glob
 import re
 import os
-# Ścieżka do folderu, w którym znajdują się pliki
-folder_path = os.getcwd() + '\data'
-
+# Path to the folder where the files are located
+folder_path = os.path.join(os.getcwd(), 'data')
+file_extension = ['xlsx', 'csv', 'txt']
+files = []
+Columns = ["Questioned_Date", "Model_No", "OS", "SW_Version", "CSC", "Category", "Application_Name", "content"]
+    
 def clean_text(text):
-    # Usunięcie znaków interpunkcyjnych
+    # Remove punctuation marks
     cleaned_text = re.sub(r"[^\w\s]", "", re.sub(r"[\t\n]", "", text))
 
     return cleaned_text
 
 def get_data(folder_path):
-    # Pobieranie plików z rozszerzeniami .xlsx, .csv i .txt
-    file_extension = ['xlsx', 'csv', 'txt']
-    files = []
+    # Download files with extensions .xlsx, .csv and .txt
     for extension in file_extension:
         files.extend(glob.glob(f"{folder_path}/*.{extension}"))
 
-    # Tworzenie pustej tabeli pandas, która będzie przechowywać scalone dane
-    merged_data = pd.DataFrame(columns=["Questioned_Date", "Model_No", "OS", "SW_Version", "CSC", "Category", "Application_Name", "content"])
-    # Iteracja przez wszystkie pliki i dodawanie ich zawartości do tabeli pandas
+    # Create an empty pandas table that will hold the merged data
+    merged_data = pd.DataFrame(columns=Columns)
+    # Iterate through all the files and add their contents to the pandas table
     for file in files:
         if file.endswith('.xlsx'):
             data = pd.read_excel(file)
-
-            # data['content'] = data['content'].apply(clean_text)
         elif file.endswith('.csv'):
             data = pd.read_csv(file, delimiter=';')
-            # data['content'] = data['content'].apply(clean_text)
         else:
             data = pd.read_csv(file, delimiter=';')
-            # data['content'] = data['content'].apply(clean_text)
-
         merged_data = pd.concat([merged_data, data], ignore_index=True, )
-    print(merged_data)
+        
     return merged_data['content'].apply(clean_text)
 
 if __name__ == "__main__":
